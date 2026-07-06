20ee734b6244bf76f92a7f608fb987d90897b02e
mapaza <monica.apazacora@gmail.com>
2026-07-06 18:28:41 -0400

    feat: add navigation fallback to redirect to home on unmatched routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,20 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import Login from './components/Login'
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={
-              <App />
-          }
-        />
+        <Route path="/" element={<App />} />
         <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
